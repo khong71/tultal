641f4f27294c5cee0054e479c026b324ec494f5a
LilithZ007 <thitiwat.tiw17@gmail.com>
2024-10-23 17:21:17 +0700

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@
 
 // ignore_for_file: prefer_const_constructors, unnecessary_null_comparison, prefer_if_null_operators
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tultal/Page/Homeraider.dart';
@@ -27,7 +28,9 @@ import 'package:tultal/Page/Homeuser.dart';
 import 'package:tultal/Page/Login.dart';
 
 void main() async {
-  await GetStorage.init(); // เริ่มต้น GetStorage
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
+  GetStorage.init(); // เริ่มต้น GetStorage
   runApp(const MyApp());
 }
 
@@ -37,15 +40,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final box = GetStorage(); // สร้าง instance ของ GetStorage
-    bool isLoggedIn = box.read('isLoggedIn') ?? false; // อ่านสถานะล็อกอิน (ค่าเริ่มต้นคือ false ถ้าไม่มีการบันทึกค่า)
-    String userType = box.read('userType') ?? ''; // ตรวจสอบประเภทของผู้ใช้ (user/raider)
-    int userId = box.read('userId') ?? 0; // อ่าน userId (ค่าเริ่มต้นคือ 0 ถ้าไม่มีการบันทึกค่า)
+    bool isLoggedIn = box.read('isLoggedIn') ??
+        false; // อ่านสถานะล็อกอิน (ค่าเริ่มต้นคือ false ถ้าไม่มีการบันทึกค่า)
+    String userType =
+        box.read('userType') ?? ''; // ตรวจสอบประเภทของผู้ใช้ (user/raider)
+    int userId = box.read('userId') ??
+        0; // อ่าน userId (ค่าเริ่มต้นคือ 0 ถ้าไม่มีการบันทึกค่า)
     int raiderId = box.read('userId') ?? 0;
 
     return MaterialApp(
       title: 'Flutter Demo',
-      home: isLoggedIn 
-          ? (userType == 'user' ? Homeuser(userId: userId) : Homeraider(raiderId: raiderId)) // ถ้าเป็น user ไปหน้า Homeuser ถ้าเป็น raider ไปหน้า Homeraider
+      home: isLoggedIn
+          ? (userType == 'user'
+              ? Homeuser(userId: userId)
+              : Homeraider(
+                  raiderId:
+                      raiderId)) // ถ้าเป็น user ไปหน้า Homeuser ถ้าเป็น raider ไปหน้า Homeraider
           : LoginPage(), // ถ้ายังไม่ล็อกอินให้ไปหน้า LoginPage
     );
   }
